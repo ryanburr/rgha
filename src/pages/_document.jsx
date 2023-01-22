128a082b2a829df14f704cd7443af945ab3237b0
Ryan M Burr <ryan.burr2@gmail.com>
2023-01-22 14:45:21 -0500

    fix: dark mode check

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,7 +9,8 @@ const modeScript = `
 
   function updateMode() {
     let isSystemDarkMode = darkModeMediaQuery.matches
-    let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
+    // let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
+    let isDarkMode = false;
 
     if (isDarkMode) {
       document.documentElement.classList.add('dark')
